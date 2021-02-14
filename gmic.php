24a2d55b6f2f61fbcf762bad38e7b2e49a412f40
O. Inha <50331907+olaviinha@users.noreply.github.com>
2021-02-14 19:12:21 +0200

    Restore last images

--- gmic.php
+++ gmic.php
@@ -1,10 +1,17 @@
 <?php
 
-// Empty old images
 $files = glob('tmp/*');
-foreach($files as $file){
-  if(is_file($file)) {
-    unlink($file);
+$action = 'list';
+
+if( isset($_POST['action']) && $_POST['action'] != ''){
+  $action = $_POST['action'];
+}
+
+if($action == 'list'){
+  if(count($files) == 0){
+    echo 0;
+  }  else {
+    echo str_replace("tmp/", "", explode(".", $files[0])[0]);
   }
 }
 
@@ -17,7 +24,7 @@ function getimg($url) {
     $process = curl_init($url);         
     curl_setopt($process, CURLOPT_HTTPHEADER, $headers);         
     curl_setopt($process, CURLOPT_HEADER, 0);         
-    curl_setopt($process, CURLOPT_USERAGENT, $user_agent); //check here         
+    curl_setopt($process, CURLOPT_USERAGENT, $user_agent);
     curl_setopt($process, CURLOPT_TIMEOUT, 30);         
     curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);         
     curl_setopt($process, CURLOPT_FOLLOWLOCATION, 1);         
@@ -27,53 +34,61 @@ function getimg($url) {
 } 
 
 // Generate images
-if (isset($_POST['action']) && $_POST['action'] == 'generate') {
-    $img_id = date("YmdHis");
-    $imgpath = "tmp/${img_id}.jpg";
-    if($_POST['isUrl'] == 'true'){
-        $imgurl = $_POST['data'];
-        copy($imgurl, $imgpath);
-    } else {
-        $extension = explode('/', mime_content_type($_POST['data']))[1];
-        $data = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $_POST['data']));
-        file_put_contents("tmp/${img_id}.jpg", $data);
+if ($action == 'generate') {
+
+  // Clear old images
+  foreach($files as $file){
+    if(is_file($file)) {
+      unlink($file);
     }
+  }
 
-    // Posterize
-    exec('gmic -input ' . $imgpath . ' -fx_posterize 150,30,1,2,0,0,1,0 -output tmp/' . $img_id . '_pz1.jpg');
-    exec('gmic -input ' . $imgpath . ' -fx_posterize 150,30,1,4,0,0,1,0 -output tmp/' . $img_id . '_pz2.jpg');
-    exec('gmic -input ' . $imgpath . ' -fx_posterize 150,30,1,6,0,0,1,0 -output tmp/' . $img_id . '_pz3.jpg');
-    exec('gmic -input ' . $imgpath . ' -fx_posterize 150,30,1,10,0,0,1,0 -output tmp/' . $img_id . '_pz4.jpg');
-    exec('gmic -input ' . $imgpath . ' -fx_posterize 150,30,1,12,0,0,1,0 -output tmp/' . $img_id . '_pz5.jpg');
-    exec('gmic -input ' . $imgpath . ' -fx_posterize 150,30,1,16,0,0,1,0 -output tmp/' . $img_id . '_pz6.jpg');
-    exec('gmic -input ' . $imgpath . ' -fx_posterize 150,30,1,20,0,0,1,0 -output tmp/' . $img_id . '_pz7.jpg');
+  $img_id = date("YmdHis");
+  $imgpath = "tmp/${img_id}.jpg";
+  if($_POST['isUrl'] == 'true'){
+      $imgurl = $_POST['data'];
+      copy($imgurl, $imgpath);
+  } else {
+      $extension = explode('/', mime_content_type($_POST['data']))[1];
+      $data = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $_POST['data']));
+      file_put_contents("tmp/${img_id}.jpg", $data);
+  }
 
-    // Vectorize
-    exec('gmic -input ' . $imgpath . ' -fx_vector_painting 3 -output tmp/' . $img_id . '_vc1.jpg');
-    exec('gmic -input ' . $imgpath . ' -fx_vector_painting 6 -output tmp/' . $img_id . '_vc2.jpg');
-    exec('gmic -input ' . $imgpath . ' -fx_vector_painting 7 -output tmp/' . $img_id . '_vc3.jpg');
-    exec('gmic -input ' . $imgpath . ' -fx_vector_painting 8 -output tmp/' . $img_id . '_vc4.jpg');
-    exec('gmic -input ' . $imgpath . ' -fx_vector_painting 9 -output tmp/' . $img_id . '_vc5.jpg');
-    exec('gmic -input ' . $imgpath . ' -fx_vector_painting 9.2 -output tmp/' . $img_id . '_vc6.jpg');
-    exec('gmic -input ' . $imgpath . ' -fx_vector_painting 9.4 -output tmp/' . $img_id . '_vc7.jpg');
-    exec('gmic -input ' . $imgpath . ' -fx_vector_painting 9.6 -output tmp/' . $img_id . '_vc8.jpg');
-    exec('gmic -input ' . $imgpath . ' -fx_vector_painting 9.8 -output tmp/' . $img_id . '_vc9.jpg');
-    exec('gmic -input ' . $imgpath . ' -fx_vector_painting 9.9 -output tmp/' . $img_id . '_vc10.jpg');
+  // Posterize
+  exec('gmic -input ' . $imgpath . ' -fx_posterize 150,30,1,2,0,0,1,0 -output tmp/' . $img_id . '_pz1.jpg');
+  exec('gmic -input ' . $imgpath . ' -fx_posterize 150,30,1,4,0,0,1,0 -output tmp/' . $img_id . '_pz2.jpg');
+  exec('gmic -input ' . $imgpath . ' -fx_posterize 150,30,1,6,0,0,1,0 -output tmp/' . $img_id . '_pz3.jpg');
+  exec('gmic -input ' . $imgpath . ' -fx_posterize 150,30,1,10,0,0,1,0 -output tmp/' . $img_id . '_pz4.jpg');
+  exec('gmic -input ' . $imgpath . ' -fx_posterize 150,30,1,12,0,0,1,0 -output tmp/' . $img_id . '_pz5.jpg');
+  exec('gmic -input ' . $imgpath . ' -fx_posterize 150,30,1,16,0,0,1,0 -output tmp/' . $img_id . '_pz6.jpg');
+  exec('gmic -input ' . $imgpath . ' -fx_posterize 150,30,1,20,0,0,1,0 -output tmp/' . $img_id . '_pz7.jpg');
 
-    // Restore details
-    exec('gmic -input ' . $imgpath . ' -fx_freaky_details 2,10,1,11,0,32,0 -output tmp/' . $img_id . '_fd.jpg');
+  // Vectorize
+  exec('gmic -input ' . $imgpath . ' -fx_vector_painting 3 -output tmp/' . $img_id . '_vc1.jpg');
+  exec('gmic -input ' . $imgpath . ' -fx_vector_painting 6 -output tmp/' . $img_id . '_vc2.jpg');
+  exec('gmic -input ' . $imgpath . ' -fx_vector_painting 7 -output tmp/' . $img_id . '_vc3.jpg');
+  exec('gmic -input ' . $imgpath . ' -fx_vector_painting 8 -output tmp/' . $img_id . '_vc4.jpg');
+  exec('gmic -input ' . $imgpath . ' -fx_vector_painting 9 -output tmp/' . $img_id . '_vc5.jpg');
+  exec('gmic -input ' . $imgpath . ' -fx_vector_painting 9.2 -output tmp/' . $img_id . '_vc6.jpg');
+  exec('gmic -input ' . $imgpath . ' -fx_vector_painting 9.4 -output tmp/' . $img_id . '_vc7.jpg');
+  exec('gmic -input ' . $imgpath . ' -fx_vector_painting 9.6 -output tmp/' . $img_id . '_vc8.jpg');
+  exec('gmic -input ' . $imgpath . ' -fx_vector_painting 9.8 -output tmp/' . $img_id . '_vc9.jpg');
+  exec('gmic -input ' . $imgpath . ' -fx_vector_painting 9.9 -output tmp/' . $img_id . '_vc10.jpg');
 
-    // Outlines
-    exec('gmic -input ' . $imgpath . ' -edges 24% -normalize 0,255 -output tmp/' . $img_id . '_ol1.jpg');
-    exec('gmic -input ' . $imgpath . ' -edges 18% -normalize 0,255 -output tmp/' . $img_id . '_ol2.jpg');
-    exec('gmic -input ' . $imgpath . ' -edges 14% -normalize 0,255 -output tmp/' . $img_id . '_ol3.jpg');
-    exec('gmic -input ' . $imgpath . ' -edges 9% -normalize 0,255 -output tmp/' . $img_id . '_ol4.jpg');
-    exec('gmic -input ' . $imgpath . ' -edges 6% -normalize 0,255 -output tmp/' . $img_id . '_ol5.jpg');
-    exec('gmic -input ' . $imgpath . ' -edges 4% -normalize 0,255 -output tmp/' . $img_id . '_ol6.jpg');
-    exec('gmic -input ' . $imgpath . ' -edges 2% -normalize 0,255 -output tmp/' . $img_id . '_ol7.jpg');
-    exec('gmic -input ' . $imgpath . ' -edges 1% -normalize 0,255 -output tmp/' . $img_id . '_ol8.jpg');
+  // Restore details
+  exec('gmic -input ' . $imgpath . ' -fx_freaky_details 2,10,1,11,0,32,0 -output tmp/' . $img_id . '_fd.jpg');
 
-    echo $img_id;
+  // Outlines
+  exec('gmic -input ' . $imgpath . ' -edges 24% -normalize 0,255 -output tmp/' . $img_id . '_ol1.jpg');
+  exec('gmic -input ' . $imgpath . ' -edges 18% -normalize 0,255 -output tmp/' . $img_id . '_ol2.jpg');
+  exec('gmic -input ' . $imgpath . ' -edges 14% -normalize 0,255 -output tmp/' . $img_id . '_ol3.jpg');
+  exec('gmic -input ' . $imgpath . ' -edges 9% -normalize 0,255 -output tmp/' . $img_id . '_ol4.jpg');
+  exec('gmic -input ' . $imgpath . ' -edges 6% -normalize 0,255 -output tmp/' . $img_id . '_ol5.jpg');
+  exec('gmic -input ' . $imgpath . ' -edges 4% -normalize 0,255 -output tmp/' . $img_id . '_ol6.jpg');
+  exec('gmic -input ' . $imgpath . ' -edges 2% -normalize 0,255 -output tmp/' . $img_id . '_ol7.jpg');
+  exec('gmic -input ' . $imgpath . ' -edges 1% -normalize 0,255 -output tmp/' . $img_id . '_ol8.jpg');
+
+  echo $img_id;
 }
 
 ?>
